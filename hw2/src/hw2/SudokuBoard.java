e31d6b47c97aa6a570ac6bf753376420a357c255
Juan <jp41011@gmail.com>
2017-04-23 22:55:49 -0700

    Started HW2 Sudoku solver

---
+++ hw2/src/hw2/SudokuBoard.java
@@ -0,0 +1,37 @@
+package hw2;
+
+/*
+Sudoku Board class
+sudoku board construct to store functions for the
+sudoku board.
+ */
+
+public class SudokuBoard {
+
+	protected int boardSize;
+	
+	// constructor
+	SudokuBoard()
+	{
+		
+	}
+	
+	// print board
+	public void print()
+	{
+		return;
+	}
+	
+	// checks to see if this entry is valid
+	// and doesn't break any sudoku rules
+	public boolean isGoodMove()
+	{
+		return false;
+	}
+	
+	// check to see if the board is solved
+	public boolean isSolved()
+	{
+		return false;
+	}
+}
---
+++ hw2/src/hw2/hw2.java
@@ -0,0 +1,24 @@
+package hw2;
+
+/**
+ Juan Pedraza - CSci 264 - HW2
+ Sudoku Solver
+ This program will read in information describing the dimensions and information
+ about a sudoku puzzle.
+ */
+
+public class hw2 {
+
+	static int DFS_Count = 0;	// count states explored with DFS
+	static int H_Count = 0;		// count states explored with heuristic approach 
+	
+	public static void main(String[] args) {
+		// Read in from input file
+		
+		// call DFS solution
+		
+		// call heuristic solution3
+
+	}
+
+}
